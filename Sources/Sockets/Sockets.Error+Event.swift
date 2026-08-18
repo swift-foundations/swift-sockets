2c84b44b2ab06fce5d1602fc80d030c6f8518e20
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-18 18:29:28 +0200

    Insert blank lines between multi-line switch cases
    vertical_whitespace_between_cases under the CI-pinned SwiftLint 0.63.3;
    newer local versions do not flag these sites.

--- Sources/Sockets/Sockets.Error+Event.swift
+++ Sources/Sockets/Sockets.Error+Event.swift
@@ -24,26 +24,36 @@ extension Sockets.Error {
         switch failure {
         case .left(.cancelled):
             self = .cancelled
+
         case .left(.shutdown):
             self = .ioShutdown
+
         case .left(.timeout):
             self = .timeout
+
         case .right(let error):
             switch error {
             case .platform(let code):
                 self.init(code: code)
+
             case .invalidDescriptor:
                 self = .descriptor(.invalid)
+
             case .alreadyRegistered:
                 self = .registration(.duplicate)
+
             case .notRegistered:
                 self = .registration(.missing)
+
             case .deregistered:
                 self = .registration(.removed)
+
             case .readClosed:
                 self = .closed(.read)
+
             case .writeClosed:
                 self = .closed(.write)
+
             case .notConnected:
                 self = .notConnected
             }
